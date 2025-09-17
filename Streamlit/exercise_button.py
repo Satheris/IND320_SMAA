467a9853cf3e6f34aec4ecd6f07f3f6462199fed
Satheris <148965732+Satheris@users.noreply.github.com>
2025-09-17 11:07:10 +0200

    Update exercise_button.py

--- Streamlit/exercise_button.py
+++ Streamlit/exercise_button.py
@@ -4,25 +4,37 @@ import numpy as np
 
 st.header('Collatz conjecture')
 
+# Initialize caching 
 if 'value' not in st.session_state:
-    st.session_state.value = 0
+    st.session_state.value = None
+    st.session_state.started = False 
+    button_label = 'Start'
+else: 
+    if st.session_state.value % 2 == 0:
+        button_label = 'Half it'
+    elif st.session_state.value == 1: 
+        button_label = 'Success!'
+    else: 
+        button_label = 'Triple and add one'
 
-def update():
-    if st.session_state.value:
-        return st.session_state.value
 
+# Button varies by state 
+st.button(button_label, disabled=(st.session_state.value == 1))
 
-@st.cache_data
-def value():
-    v = np.random.randint(1, 101)
-
-
-button = st.button('Start')
-
-if not button:
+# Output 
+if not st.session_state.started: 
     st.write('Ready')
+else:
+    st.write(st.session_state.value)
 
-if button:
-    v = value()
-    button = st.button('Next')
-    st.write(v)
+# Change in values 
+if not st.session_state.started:
+    # First press: sample random integer and cache it
+    st.session_state.started = True 
+    st.session_state.value = np.random.randint(1, 101)
+else:
+    # Subsequent presses: update the value according to the rules
+    if st.session_state.value % 2 == 0:
+        st.session_state.value //= 2            # // integer division   
+    else:
+        st.session_state.value = int((st.session_state.value * 3) + 1)
