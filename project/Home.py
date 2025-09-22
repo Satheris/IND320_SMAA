05e109f709ab61e427a59d7ff0f7b6d1c5a8e3ee
Satheris <148965732+Satheris@users.noreply.github.com>
2025-09-22 10:59:31 +0200

    displaying data table on page 2

--- project/Home.py
+++ project/Home.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.header('This is my app!')
+
+@st.cache_data
+def read_data():
+    data = pd.read_csv('data/open-meteo-subset.csv')
+    return data
+
+st.session_state.data = read_data()
--- project/pages/2_Page 2.py
+++ project/pages/2_Page 2.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.header('Page 2')
+
+st.write(st.session_state.data)
