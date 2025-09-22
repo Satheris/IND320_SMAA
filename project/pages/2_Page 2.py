1af4420e58ccce7d3a8a7244afa055c1886c3bbb
Satheris <148965732+Satheris@users.noreply.github.com>
2025-09-22 12:21:07 +0200

    trying something else for linechartcolumn

--- project/pages/2_Page 2.py
+++ project/pages/2_Page 2.py
@@ -13,7 +13,10 @@ st.header('Page 2')
 
 st.dataframe(st.session_state.data)
 
-st.data_editor(data=st.session_state.data, column_config={st.column_config.LineChartColumn()})
+for column in st.session_state.data:
+    st.data_editor(data=st.session_state.data, 
+                   column_config={column:st.column_config.LineChartColumn()}, 
+                   hide_index=False)
 
 
 # st.line_chart()
