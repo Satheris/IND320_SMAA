25784afef864ccb81fc36be071d07d74e2652876
Satheris <148965732+Satheris@users.noreply.github.com>
2025-09-22 11:02:06 +0200

    Update Home.py

--- project/Home.py
+++ project/Home.py
@@ -5,7 +5,7 @@ st.header('This is my app!')
 
 @st.cache_data
 def read_data():
-    data = pd.read_csv('data/open-meteo-subset.csv')
+    data = pd.read_csv('project/data/open-meteo-subset.csv')
     return data
 
 st.session_state.data = read_data()
