4ae6f55a7816992153f6442af4431eda1faf0776
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-06 19:29:11 +0200

    Made select slider with months

--- project/pages/3_Page 3.py
+++ project/pages/3_Page 3.py
@@ -15,3 +15,18 @@ column = st.selectbox('Select column',
                       ("temperature_2m (°C)", "precipitation (mm)", 
                        "wind_speed_10m (m/s)", "wind_gusts_10m (m/s)", 
                        "wind_direction_10m (°)", 'All'))
+
+startMonth, endMonth = st.select_slider('Select months', 
+                                        options=['January',
+                                                 'February',
+                                                 'March',
+                                                 'April', 
+                                                 'May',
+                                                 'June',
+                                                 'July',
+                                                 'August',
+                                                 'September',
+                                                 'October',
+                                                 'November',
+                                                 'December'
+                                                 ])
