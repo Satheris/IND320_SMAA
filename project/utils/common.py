37e7950c58aaf55e730d6a42f66c7a22ecfa0c53
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-24 19:30:32 +0200

    Update common.py

---
+++ project/utils/common.py
@@ -0,0 +1,11 @@
+# utils/common.py
+import streamlit as st
+import pandas as pd
+
+
+@st.cache_data(show_spinner=True)
+def read_data() -> pd.DataFrame:
+    data = pd.read_csv('project/data/open-meteo-subset.csv')
+    data['time'] = pd.to_datetime(data['time'])
+    return data
+
