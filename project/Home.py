dbfb250a94cce6b9d4790d24014c6147cbc9c6c2
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-24 19:40:33 +0200

    Update Home.py

--- project/Home.py
+++ project/Home.py
@@ -2,14 +2,16 @@
 import streamlit as st
 import pandas as pd
 
-st.header('This is my app!')
 
-# chaching to make experience smoother
-@st.cache_data
-def read_data():
-    data = pd.read_csv('project/data/open-meteo-subset.csv')
-    data['time'] = pd.to_datetime(data['time'])
-    return data
+st.set_page_config(page_title='This is my app!', layout="wide")
 
-if 'data' not in st.session_state:
-    st.session_state.data = read_data()
+
+# # chaching to make experience smoother
+# @st.cache_data
+# def read_data():
+#     data = pd.read_csv('project/data/open-meteo-subset.csv')
+#     data['time'] = pd.to_datetime(data['time'])
+#     return data
+
+# if 'data' not in st.session_state:
+#     st.session_state.data = read_data()
