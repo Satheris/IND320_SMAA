f1c5e708223f44a2ff124dae3866deae3fcf19f2
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-24 20:01:57 +0200

    Connection to MongoDB

--- project/pages/4_Elhub.py
+++ project/pages/4_Elhub.py
@@ -2,10 +2,24 @@
 import streamlit as st
 import pandas as pd
 
-from utils.common import read_data
+
+from utils.common import (read_data, 
+                          init_connection, 
+                          get_data)
 
 if 'data' not in st.session_state:
     st.session_state.data = read_data()
 
 
 st.header('Elhub')
+
+
+# Initialize connection
+client = init_connection()
+
+# Pull data from the collection
+items = get_data(client)
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} is {item['age']} years old")
--- project/utils/common.py
+++ project/utils/common.py
@@ -1,6 +1,7 @@
 # utils/common.py
 import streamlit as st
 import pandas as pd
+import pymongo
 
 
 @st.cache_data(show_spinner=True)
@@ -9,3 +10,16 @@ def read_data() -> pd.DataFrame:
     data['time'] = pd.to_datetime(data['time'])
     return data
 
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(st.secrets["mongo"]["uri"])
+
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data(client):
+    db = client['example']
+    collection = db['data']
+    items = collection.find()
+    items = list(items)
+    return items
