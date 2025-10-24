c6db9fe103a9869de0a89cfd6bb1dab0cff04841
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-24 19:44:48 +0200

    Update Home.py

--- project/Home.py
+++ project/Home.py
@@ -5,15 +5,4 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-
 st.header('This is my app!')
-
-# # chaching to make experience smoother
-# @st.cache_data
-# def read_data():
-#     data = pd.read_csv('project/data/open-meteo-subset.csv')
-#     data['time'] = pd.to_datetime(data['time'])
-#     return data
-
-# if 'data' not in st.session_state:
-#     st.session_state.data = read_data()
