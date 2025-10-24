8f598053fe55e84987bf0097976dc75856e5e76a
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-24 20:07:08 +0200

    Update 4_Elhub.py

--- project/pages/4_Elhub.py
+++ project/pages/4_Elhub.py
@@ -1,11 +1,10 @@
 # necessary imports for this page
 import streamlit as st
 import pandas as pd
+import pymongo
 
 
-from utils.common import (read_data, 
-                          init_connection, 
-                          get_data)
+from utils.common import (read_data)
 
 if 'data' not in st.session_state:
     st.session_state.data = read_data()
@@ -14,8 +13,24 @@ if 'data' not in st.session_state:
 st.header('Elhub')
 
 
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(st.secrets["mongo"]["uri"])
+
 # Initialize connection
 client = init_connection()
+
+
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data(_client):
+    db = _client['example']
+    collection = db['data']
+    items = collection.find()
+    items = list(items)
+    return items
+
 
 # Pull data from the collection
 items = get_data(client)
