453587d25d623cfbc02d4234363fbc2396b0b137
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-26 15:58:34 +0100

    function for month end conversion

--- project/pages/3_📈_Plots.py
+++ project/pages/3_📈_Plots.py
@@ -5,7 +5,8 @@ import plotly.express as px
 
 from utils.common import (read_data, 
                           generate_months,
-                          month_start_converter)
+                          month_start_converter,
+                          month_end_converter)
 
 st.set_page_config(layout="wide")
 
@@ -31,31 +32,14 @@ startMonth, endMonth = st.select_slider('Select months',
                                         options=months,
                                                  value=(months[0], months[0]))
 
-#making dictionaries to convert months into datetime values
-
-
-endConverter = {'January': '2020-02-01',
-                'February': '2020-03-01',
-                'March': '2020-04-01',
-                'April': '2020-05-01', 
-                'May': '2020-06-01',
-                'June': '2020-07-01',
-                'July': '2020-08-01',
-                'August': '2020-09-01',
-                'September': '2020-10-01',
-                'October': '2020-11-01',
-                'November': '2020-12-01',
-                'December': '2021-01-01'
-                  }
-
 
 # using values given above to make plots
 if column == 'all':
-    fig = px.line(df[(df['time'] >= month_start_converter(startMonth)) & (df['time'] < endConverter[endMonth])], 
+    fig = px.line(df[(df['time'] >= month_start_converter(startMonth)) & (df['time'] < month_end_converter(endMonth))], 
                   x='time', y=df.columns.drop('time'), title='All weather parameters over time')
     st.plotly_chart(fig)
 
 else: 
-    fig = px.line(df[(df['time'] >= month_start_converter(startMonth)) & (df['time'] < endConverter[endMonth])], 
+    fig = px.line(df[(df['time'] >= month_start_converter(startMonth)) & (df['time'] < month_end_converter(endMonth))], 
                   x='time', y=column, title=f'{column} over time')
     st.plotly_chart(fig)
