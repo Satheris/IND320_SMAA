c2570034fca76fc1302ce53cb138ab55bf2c8fa9
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-26 23:42:05 +0100

    Filling in comments

--- project/pages/4_⚡️_Elhub.py
+++ project/pages/4_⚡️_Elhub.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pymongo
 import plotly.express as px
 
+# importing self defined functions
 from utils.common import (generate_months,
                           month_number_converter)
 
@@ -20,6 +21,7 @@ def init_connection():
 
 client = init_connection()
 
+
 # Pull data from the collection.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
@@ -31,7 +33,10 @@ def get_data():
     return items
 
 items = get_data()
+client.close() # Close connection to MongoDB
 
+
+# Converting data to dataframe and doing type conversion
 df = pd.DataFrame(items)
 df['startTime'] = pd.to_datetime(df['startTime'], errors='coerce', utc=True)
 df['quantityKwh'] = pd.to_numeric(df['quantityKwh'], errors='coerce')
@@ -39,8 +44,6 @@ df['month'] = df['startTime'].dt.month
 df['year'] = df['startTime'].dt.year
 
 df = df[df['year'] == 2021]
-
-# st.dataframe(df.head())
 
 
 # Initializing columns
@@ -50,15 +53,18 @@ c1, c2 = st.columns(2, gap='medium')
 with c1:
     st.subheader('Total energy production in 2021 by price area')
 
+    # Initiating radio selection for price areas
     areas = sorted(df['priceArea'].unique().tolist())
     area = st.radio('Choose a geographic area', areas, horizontal=True)
 
+    # Making reduced dataset
     df_kwh_byArea = df[df['priceArea'] == area]\
         .groupby('productionGroup')\
             .agg({'quantityKwh': 'sum'})\
                 .sort_values('quantityKwh', ascending=False)\
                     .reset_index()
-        
+
+    # try, except block to catch errors without crashing  
     try:
         # Base figure
         fig = px.pie(
@@ -84,17 +90,21 @@ with c1:
 with c2:
     st.subheader('Energy production by month')
     
+    # Initiating pill selection for production groups
     prods = sorted(df['productionGroup'].unique().tolist())
     prodgroups = st.pills('Select production group(s)', prods, selection_mode='multi', default=prods)
 
+    # Initiating selectbox for month selction
     months = generate_months()
     month = st.selectbox('Select month', months)
 
+    # Reducing dataset
     df_month = df[(df['priceArea'] == area) & 
                   (df['month'] == month_number_converter(month)) &
                   (df['productionGroup'].isin(prodgroups))]
     df_month = df_month.sort_values(by='productionGroup').sort_values(by='startTime').reset_index()
 
+    # try, except block to catch errors without crashing  
     try: 
         fig = px.line(df_month, 
                         x='startTime', 
@@ -107,9 +117,10 @@ with c2:
     except Exception as e:
         st.error(f'Error creating chart: {str(e)}')
 
+# End columns
 
 
-
+# Expander with info on the data
 expander = st.expander('Source of the data')
 expander.write('''
     The plots are based on data collected from Elhub Energy Data API, \
