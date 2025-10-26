11f2b41acaf0a72c6a0542edd6a3457e36079448
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-26 13:10:35 +0100

    Update 4_Elhub.py

--- project/pages/4_Elhub.py
+++ project/pages/4_Elhub.py
@@ -13,24 +13,24 @@ if 'data' not in st.session_state:
 st.header('Elhub')
 
 
+# Initialize connection.
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
     return pymongo.MongoClient(st.secrets["mongo"]["uri"])
 
+client = init_connection()
 
+# Pull data from the collection.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def get_data():
-    client = init_connection()
     db = client['example']
     collection = db['data']
     items = collection.find()
     items = list(items)
     return items
 
-
-# Pull data from the collection
 items = get_data()
 
 # Print results.
