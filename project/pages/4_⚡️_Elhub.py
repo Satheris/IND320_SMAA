110d6c2a60d57ffaad0f5c65dfdc29566d2ca3e5
Satheris <148965732+Satheris@users.noreply.github.com>
2025-10-27 12:22:08 +0100

    Update 4_⚡️_Elhub.py

--- project/pages/4_⚡️_Elhub.py
+++ project/pages/4_⚡️_Elhub.py
@@ -15,7 +15,7 @@ st.header('Elhub')
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-# @st.cache_resource
+@st.cache_resource
 def init_connection():
     return pymongo.MongoClient(st.secrets['mongo']['uri'])
 
