87dfe94a7b85496d4fc1e7717c2148c740564d50
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-06 23:35:11 +0100

    Update 6_🚩_Outliers and Anomalies.py

--- project/pages/6_🚩_Outliers and Anomalies.py
+++ project/pages/6_🚩_Outliers and Anomalies.py
@@ -41,8 +41,11 @@ with tab1:
     columns = sorted(df.columns.tolist())
     column = st.radio('Choose a weather variable', columns)
 
-    dct_cutoff = st.slider('Cutoff for DCT filter', 0, 25, value=10, step=1)
-    n_std = st.slider('Number of standard deviations for calculating upper and lower bounds', 0.5, 6.0, value=3.0, step=0.5)
+    c1, c2 = st.columns()
+    with c1:
+        dct_cutoff = st.slider('Cutoff for DCT filter', 0, 25, value=10, step=1)
+    with c2: 
+        n_std = st.slider('Number of standard deviations for calculating upper and lower bounds', 0.5, 6.0, value=3.0, step=0.5)
 
     SPC_outlier_plot(df=df, column=column, dct_cutoff=dct_cutoff, n_std=n_std)
 
