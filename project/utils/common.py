9b945534bed288b681affd439c37813ade388fff
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-06 22:12:14 +0100

    Update common.py

--- project/utils/common.py
+++ project/utils/common.py
@@ -103,32 +103,32 @@ def generate_months() -> list:
     return months
 
 def month_start_converter(month_text: str) -> str:
-    month_dict = {'January': '2020-01-01',
-                    'February': '2020-02-01',
-                    'March': '2020-03-01',
-                    'April': '2020-04-01', 
-                    'May': '2020-05-01',
-                    'June': '2020-06-01',
-                    'July': '2020-07-01',
-                    'August': '2020-08-01',
-                    'September': '2020-09-01',
-                    'October': '2020-10-01',
-                    'November': '2020-11-01',
-                    'December': '2020-12-01'}
+    month_dict = {'January': '2021-01-01',
+                    'February': '2021-02-01',
+                    'March': '2021-03-01',
+                    'April': '2021-04-01', 
+                    'May': '2021-05-01',
+                    'June': '2021-06-01',
+                    'July': '2021-07-01',
+                    'August': '2021-08-01',
+                    'September': '2021-09-01',
+                    'October': '2021-10-01',
+                    'November': '2021-11-01',
+                    'December': '2021-12-01'}
     return month_dict[month_text]
 
 def month_end_converter(month_text: str) -> str:
-    month_dict = {'January': '2020-02-01',
-                    'February': '2020-03-01',
-                    'March': '2020-04-01',
-                    'April': '2020-05-01', 
-                    'May': '2020-06-01',
-                    'June': '2020-07-01',
-                    'July': '2020-08-01',
-                    'August': '2020-09-01',
-                    'September': '2020-10-01',
-                    'October': '2020-11-01',
-                    'November': '2020-12-01',
+    month_dict = {'January': '2021-02-01',
+                    'February': '2021-03-01',
+                    'March': '2021-04-01',
+                    'April': '2021-05-01', 
+                    'May': '2021-06-01',
+                    'June': '2021-07-01',
+                    'July': '2021-08-01',
+                    'August': '2021-09-01',
+                    'September': '2021-10-01',
+                    'October': '2021-11-01',
+                    'November': '2021-12-01',
                     'December': '2021-01-01'}
     return month_dict[month_text]
 
