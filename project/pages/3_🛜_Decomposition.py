0a84c9349591a45ca5a5a3688d1c0da6880566a0
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-07 09:23:15 +0100

    Update 3_🛜_Decomposition.py

--- project/pages/3_🛜_Decomposition.py
+++ project/pages/3_🛜_Decomposition.py
@@ -11,18 +11,27 @@ from utils.common import (openmeteo_download)
 # session_state.area to use across pages for data extraction
 if 'area' not in st.session_state:
     st.session_state.area = 'NO1'
+# assigning session_state.data if not in cache
+if 'data' not in st.session_state:
+    st.session_state.data = openmeteo_download(area=st.session_state.AREA)
+
+# storing data on this page for further use
+df = st.session_state.data
 
 
 # page configuration
 st.set_page_config(layout='wide')
 st.header('Decomposition')
+st.write(f'Decomposition analyses of weather data from electrical price area {st.session_state.AREA}')
 
 
-# tab1, tab2 = st.tabs(["Cat", "Dog"])
+# initializing tabs 
+tab1, tab2 = st.tabs(['STL analysis', 'Spectrogram'])
 
-# with tab1:
-#     st.header("A cat")
-#     st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-# with tab2:
-#     st.header("A dog")
-#     st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+# filling tab 1
+with tab1:
+    st.header('Seasonal-Trend decomposition with LOESS')
+
+# filling tab 2
+with tab2:
+    st.header('Spectrogram')
