c6aced91aca4a4d586f925d7ba0023811586ed6d
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-07 10:45:53 +0100

    Update common.py

--- project/utils/common.py
+++ project/utils/common.py
@@ -235,7 +235,7 @@ def STL_plotter(df_elhub, area='NO1', prodGroup='wind', periodLength=12,
             row=4, col=1)
     
     fig.update_layout(
-        margin=dict(l=40, r=40, t=20, b=20),
+        margin=dict(l=60, r=60, t=20, b=20),
 )
 
     st.plotly_chart(fig)
