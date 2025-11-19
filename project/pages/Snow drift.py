d39a7c22a918294ffcb32241bf16fcf0cf63735e
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-19 12:12:42 +0100

    Update Snow drift.py

---
+++ project/pages/Snow drift.py
@@ -0,0 +1,19 @@
+# necessary imports for this page
+import streamlit as st
+
+# Importing self defined functions
+from utils.common import (openmeteo_download,
+                          openmeteo_download_snowdrift)
+from utils.snowdrift import snowdrift_plot
+
+
+# session_state.area to use across pages for data extraction
+if 'AREA' not in st.session_state:
+    st.session_state.AREA = 'NO1'
+# assigning session_state.weather_data if not in cache
+if 'weather_data' not in st.session_state:
+    st.session_state.weather_data = openmeteo_download(area=st.session_state.AREA)
+
+weather_data_snow = openmeteo_download_snowdrift(area=st.session_state.AREA)
+
+snowdrift_plot(weather_data_snow)
