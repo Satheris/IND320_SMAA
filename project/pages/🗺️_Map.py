1637dfb575fa4f6a8d10621c0b1aae32667b5dfd
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-20 13:45:12 +0100

    Update 🗺️_Map.py

--- project/pages/🗺️_Map.py
+++ project/pages/🗺️_Map.py
@@ -6,9 +6,9 @@ from utils.common import (map_outline)
 from utils.snowdrift import snowdrift_plot
 
 
-# assigning session_state.location if not in cache
-if 'location' not in st.session_state:
-    st.session_state.location = None
+# # assigning session_state.location if not in cache
+# if 'location' not in st.session_state:
+#     st.session_state.location = None
 
 
 # page configuration
@@ -20,7 +20,7 @@ st.write(f"Map covering Norway's electrical price areas")
 c1, c2 = st.columns(2, gap='medium')
 
 with c1: 
-    st.plotly_chart(map_outline(), key='location', on_select='rerun')
+    st.plotly_chart(map_outline())#, key='location', on_select='rerun')
 
 with c2:
     st.write(st.session_state.location)
