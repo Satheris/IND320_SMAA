b20d94bca83b36509cc43221339b07061f470a8b
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-20 14:17:37 +0100

    Update common.py

--- project/utils/common.py
+++ project/utils/common.py
@@ -354,8 +354,8 @@ def map_outline(df=None):
     
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
                       coloraxis_showscale=False,  # Explicitly hide color scale in layout
-                      height=575,  # Good height for Norway's shape
-                      width=450    # Narrow width matches Norway's orientation
+                      height=575,
+                      width=450
                       )
 
     return fig
