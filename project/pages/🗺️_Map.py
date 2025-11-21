96fd170db038247f21309834792082e825b66256
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-21 16:01:21 +0100

    Update 🗺️_Map.py

--- project/pages/🗺️_Map.py
+++ project/pages/🗺️_Map.py
@@ -60,7 +60,7 @@ try:
             st.session_state.selected_region_feature,
             style_function=lambda feature: {
                 'fillColor': 'orange',
-                'color': "red",
+                'color': "rgba(232, 134, 42, 255)", #rgba(0,0,0,0)
                 'weight': 4,  # Thicker border for emphasis
                 'fillOpacity': 0, 
                 'dashArray': '0'  # Ensure no dashes
