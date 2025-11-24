69cb7f0ee4707adbe4e85cf499e82441e8f2c8d8
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-24 10:19:21 +0100

    Update 🗺️_Map.py

--- project/pages/🗺️_Map.py
+++ project/pages/🗺️_Map.py
@@ -215,5 +215,5 @@ with c2:
     energy_type = st.pills('Select energy type', ['production', 'consumption'], selection_mode='single', default=None, key='energy_type')
 
     if energy_type:
-        group = st.selectbox(f'Select {energy_type} group', st.session_state[energy_type+'_data'][energy_type+'Group'].unique().sorted(), 
+        group = st.selectbox(f'Select {energy_type} group', st.session_state[energy_type+'_data'][energy_type+'Group'].unique().sort(), 
                              key='group',)
