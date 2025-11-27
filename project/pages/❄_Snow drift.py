5d698009bbf3e60e612917d1759ea8603a4a1237
Satheris <148965732+Satheris@users.noreply.github.com>
2025-11-27 21:15:09 +0100

    Update ❄_Snow drift.py

--- project/pages/❄_Snow drift.py
+++ project/pages/❄_Snow drift.py
@@ -31,15 +31,23 @@ st.set_page_config(layout='wide')
 st.header('Snow Drift analysis')
 st.write(f'Snow drift direction diagram for location chosen on *map page*')
 
-c1, c2 = st.columns(2)
-with c1:
-    start_year = st.number_input('Start year', min_value=2000, max_value=2023, 
-                                 value=st.session_state.START_YEAR, step=1, 
-                                 key='start_year', on_change=_set_new_start_year)
-with c2:
-    end_year = st.number_input('End year', min_value=2001, max_value=2024, 
-                               value=st.session_state.END_YEAR, step=1, 
-                               key='end_year', on_change=_set_new_end_year)
+# c1, c2 = st.columns(2)
+# with c1:
+#     start_year = st.number_input('Start year', min_value=2000, max_value=2023, 
+#                                  value=st.session_state.START_YEAR, step=1, 
+#                                  key='start_year', on_change=_set_new_start_year)
+# with c2:
+#     end_year = st.number_input('End year', min_value=2001, max_value=2024, 
+#                                value=st.session_state.END_YEAR, step=1, 
+#                                key='end_year', on_change=_set_new_end_year)
+
+start_year, end_year = st.select_slider('Select year range for snow drift calculation',
+                                        [i for i in range(2000, 2025, 1)],
+                                        value=(2021, 2022),
+                                        key='year_range',
+                                        )
+if start_year == end_year:
+    st.error('Error: year range has to span at least two years.')
 
 try:
     snowdrift_plot()
